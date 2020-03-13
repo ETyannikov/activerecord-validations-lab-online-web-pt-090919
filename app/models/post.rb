6f5410a015e7908445e499d7928fc7c6bd0f1a0d
Eddy Tyannikov <etyannikov@gmail.com>
2020-03-13 22:06:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :isClickbait
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  
+  def isClickbait
+    
+  end
 end
