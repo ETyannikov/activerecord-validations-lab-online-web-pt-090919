0851368a49164f7b4f32c81dff6ac0ccba64202c
Eddy Tyannikov <etyannikov@gmail.com>
2020-03-13 22:09:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   
   def isClickbait
-    errors.add unless :title.include?"Won't Believe"||"Secret"||"Top [number]"||"Guess"
+    errors.add(:title) unless :title.include?"Won't Believe"||"Secret"||"Top [number]"||"Guess"
   end
 end
